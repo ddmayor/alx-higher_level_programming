1f6a3489541104a0c524a39db17b70791901bfdb
ddmayor <105300138+ddmayor@users.noreply.github.com>
2023-01-12 01:17:53 +0100

    python

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """Returns a set of commmon elements in two sets
+
+    Args:
+        set_1 (set): set to check
+        set_2 (set): set to check
+    """
+    return (set_1 & set_2)
